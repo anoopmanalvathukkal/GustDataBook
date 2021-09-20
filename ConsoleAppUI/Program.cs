813fbe20fbc0f43e91dc4a489ed705bcd00a885a
manalvathukkal <manalvathukkal.anoop@gmail.com>
2021-09-20 19:28:00 +0900

    Add project files.

---
+++ ConsoleAppUI/Program.cs
@@ -0,0 +1,86 @@
+﻿using GustData.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppUI
+{
+    class Program
+    {
+
+        public static List<PersonModel> peopels = new List<PersonModel>();
+
+        static void Main(string[] args)
+        {
+
+            //get all gust information
+            GetGustAllInformation();
+
+            //display all data
+            DisplayUserInformations();
+
+            //
+            Console.ReadKey(true);
+
+        }
+
+        private static string GetGustData(string userMessage)
+        {
+            string output = "";
+
+            Console.Write($"{userMessage} ");
+            output = Console.ReadLine();
+
+            return output;
+        }
+
+        private static void GetGustAllInformation()
+        {
+            string moreUserComing = "";
+
+            do
+            {
+                // initialize person data
+                PersonModel person = new PersonModel();
+
+                person.FristName = GetGustData("Enter user firstName");
+                person.LastName = GetGustData("Enter user lastName");
+                person.Message = GetGustData("Enter user message to host");
+
+                Console.WriteLine("-----------------------------------");
+                Console.Write("Are more gust coming yes/no ");
+                moreUserComing = Console.ReadLine();
+
+                //add data to list
+                peopels.Add(person);
+                Console.Clear();
+
+            } while (moreUserComing.ToLower() == "yes");
+        }
+
+        private static void DisplayUserInformations()
+        {
+            Console.Clear();
+
+            if (peopels.Count > 0)
+            {
+                foreach (var item in peopels)
+                {
+                    Console.WriteLine(item.FristName);
+                    Console.WriteLine(item.LastName);
+                    Console.WriteLine(item.Message);
+                    Console.WriteLine("-----------------------");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No data has been added!");
+            }
+
+
+        }
+    }
+}
---
+++ GustData/Model/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GustData.Model
+{
+    public class PersonModel
+    {
+        //initial data
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public string Message { get; set; }
+
+    }
+}
